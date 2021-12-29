418a737ed94805fbf90afb26ef5b26d0fd7dba80
James Hudnall <jimmyhudnall@gmail.com>
2021-12-28 20:13:15 -0500

    Implement working localStorage functionality

--- 4-DOM-Manipulation/4.4-Todos-Exercise/app.js
+++ 4-DOM-Manipulation/4.4-Todos-Exercise/app.js
@@ -1,37 +1,67 @@
 const form = document.querySelector('form');
 const list = document.querySelector('#todos');
 const todoInput = document.querySelector('input[id="newTodo"]');
+const liStorageList = [];
 
-form.addEventListener("submit", function(e) {
+const populateLocalStorage = function () {
+  if(localStorage.todos) {
+    const todos = JSON.parse(localStorage.todos);
+    for (let todo of todos) {
+      appendNewTodo(todo);
+    }
+  }
+};
+
+const appendNewTodo = function (str = '') {
+  const li = document.createElement("li");
+  str ? li.innerText = str : li.innerText = todoInput.value;
+  sendToLocalStorage(li.innerText);
+  const removeBtn = document.createElement("button");
+  removeBtn.innerText = 'X';
+  li.append(removeBtn);
+  list.append(li);
+  todoInput.value = '';
+};
+
+const sendToLocalStorage = function (text) {
+  liStorageList.push(text);
+  const convertedLI = JSON.stringify(liStorageList);
+  localStorage.setItem('todos', convertedLI);
+};
+
+const removeFromLocalStorage = function (text) {
+  let idx = liStorageList.indexOf(text);
+  if (idx !== -1) {
+    liStorageList.splice(idx, 1);
+    const convertedLI = JSON.stringify(liStorageList);
+    localStorage.setItem('todos', convertedLI);
+  }
+}
+
+const removeTodo = function (e) {
+  const target = e.target.parentElement.childNodes[0].textContent;
+  e.target.parentElement.remove();
+  removeFromLocalStorage(target);
+};
+
+const checkItem = function (e) {
+  e.target.classList.toggle('checked');
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  populateLocalStorage();
+})
+
+// Listeners
+form.addEventListener("submit", function (e) {
   e.preventDefault();
   appendNewTodo();
 })
 
-list.addEventListener("click", function(e) {
-  // if target === 'BUTTON' => removeTodo
-  // else if target === "LI" => toggleClass
-  // console.log(e.target);
+list.addEventListener("click", function (e) {
   if (e.target.tagName === 'BUTTON') {
     removeTodo(e);
   } else if (e.target.tagName === 'LI') {
     checkItem(e);
   }
 })
-
-const appendNewTodo = function() {
-  const li = document.createElement("li");
-  li.innerText = todoInput.value;
-  const removeBtn = document.createElement("button");
-  removeBtn.innerText = 'X';
-  li.append(removeBtn);
-  list.append(li);
-  todoInput.value = '';
-}
-
-const removeTodo = function(e) {
-  e.target.parentElement.remove();
-}
-
-const checkItem = function(e) {
-  e.target.classList.toggle('checked');
-}
