1a0335c19791f7e32e8893cf1078e178053e1ab8
James Hudnall <jimmyhudnall@gmail.com>
2021-12-29 08:48:34 -0500

    Implement Part 2 functionality

--- 4-DOM-Manipulation/4.5-Memory-Game/script.js
+++ 4-DOM-Manipulation/4.5-Memory-Game/script.js
@@ -1,4 +1,5 @@
 const gameContainer = document.getElementById("game");
+let nextCardColor = null;
 
 const COLORS = [
   "red",
@@ -13,9 +14,8 @@ const COLORS = [
   "purple"
 ];
 
-// here is a helper function to shuffle an array
-// it returns the same array with values shuffled
-// it is based on an algorithm called Fisher Yates if you want ot research more
+let shuffledColors = shuffle(COLORS);
+
 function shuffle(array) {
   let counter = array.length;
 
@@ -27,27 +27,16 @@ function shuffle(array) {
     array[counter] = array[index];
     array[index] = temp;
   }
-
   return array;
 }
 
-let shuffledColors = shuffle(COLORS);
 
-// this function loops over the array of colors
-// it creates a new div and gives it a class with the value of the color
-// it also adds an event listener for a click for each card
 function createDivsForColors(colorArray) {
   for (let color of colorArray) {
-    // create a new div
     const newDiv = document.createElement("div");
-
-    // give it a class attribute for the value we are looping over
     newDiv.classList.add(color);
-
-    // call a function handleCardClick when a div is clicked on
+    newDiv.setAttribute("data-clicked", false);
     newDiv.addEventListener("click", handleCardClick);
-
-    // append the div to the element with an id of game
     gameContainer.append(newDiv);
   }
 }
@@ -55,8 +44,39 @@ function createDivsForColors(colorArray) {
 // TODO: Implement this function!
 function handleCardClick(event) {
   // you can use event.target to see which element was clicked
-  console.log("you just clicked", event.target);
+  let color = event.target.className;
+  if (event.target.dataset.clicked === "false") {
+    event.target.style.backgroundColor = color;
+    event.target.dataset.clicked = true;
+
+    if (nextCardColor === color) {
+      nextCardColor = null;
+    } else if (nextCardColor === null) {
+      nextCardColor = color;
+    } else {
+      resetCards(event, color);
+    }
+  }
 }
 
-// when the DOM loads
+function resetCards(event, color) {
+  // Change background color to the class of the card clicked (event.target)
+  event.target.style.backgroundColor = color;
+  // Use setTimeout to "reset" current and last card
+  setTimeout(function () {
+    event.target.style.backgroundColor = "";
+    event.target.dataset.clicked = false;
+    const cardsOfGivenStyle = document.getElementsByClassName(nextCardColor);
+    for (let card of cardsOfGivenStyle) {
+      card.style.backgroundColor = "";
+      card.dataset.clicked = false;
+    }
+    nextCardColor = null;
+  }, 1000)
+}
+
 createDivsForColors(shuffledColors);
+
+// Notes / TODO:
+// Remove user ability to "spam click"
+//Need to fix: If user correctly clicks a third card during setTimeout, it registers as a match.
