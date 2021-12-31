9b8e6ad3fee0ee8b14b8f2338ec0fc4de4229b43
James Hudnall <jimmyhudnall@gmail.com>
2021-12-31 16:13:04 -0500

    Implement solution and tests for calculator exercise

--- 7-Jasmine-Testing/7.1-Jasmine-Testing-Exercises/calculator/calculator-test.js
+++ 7-Jasmine-Testing/7.1-Jasmine-Testing-Exercises/calculator/calculator-test.js
@@ -1,11 +1,46 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  const values = {
+    amount: 100000,
+    years: 30,
+    rate: 5
+  };
+  expect(calculateMonthlyPayment(values)).toBe(536.82);
+});
+
+it('should calculate the monthly rate correctly', function () {
+  const values = {
+    amount: 47500,
+    years: 10,
+    rate: 3.5
+  };
+  expect(calculateMonthlyPayment(values)).toBe(469.71);
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  const values = {
+    amount: 100000,
+    years: 30,
+    rate: 5
+  };
+  expect(calculateMonthlyPayment(values)).not.toBe(536.821);
 });
 
-/// etc
+it("should handle interest rate of 0", function() {
+  const values = {
+    amount: 100000,
+    years: 30,
+    rate: 0
+  };
+  expect(calculateMonthlyPayment(values)).toBe("Invalid Input");
+});
+
+it("should handle term value of 0", function() {
+  const values = {
+    amount: 100000,
+    years: 0,
+    rate: 5
+  };
+  expect(calculateMonthlyPayment(values)).toBe("Invalid Input");
+});
--- 7-Jasmine-Testing/7.1-Jasmine-Testing-Exercises/calculator/calculator.js
+++ 7-Jasmine-Testing/7.1-Jasmine-Testing-Exercises/calculator/calculator.js
@@ -1,8 +1,8 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
   const form = document.getElementById("calc-form");
   if (form) {
     setupIntialValues();
-    form.addEventListener("submit", function(e) {
+    form.addEventListener("submit", function (e) {
       e.preventDefault();
       update();
     });
@@ -21,20 +21,45 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  const loan = document.getElementById("loan-amount");
+  const term = document.getElementById("loan-years")
+  const rate = document.getElementById("loan-rate");
+
+  loan.value = 100000;
+  term.value = 30;
+  rate.value = 5;
+
+  update();
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  const values = getCurrentUIValues();
+  const payment = calculateMonthlyPayment(values);
+  updateMonthly(payment);
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  let rate = values.rate / 100 / 12;
+  let n = values.years * 12;
+  let amount = values.amount;
+  let payment = (amount * rate) / (1 - (1 + rate) ** (-n));
+
+  if (isNaN(payment) || payment === Infinity) {
+    return 'Invalid Input'
+  }
+  payment = Math.round(payment * 100) / 100;
+  return payment;
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  const paymentSpan = document.querySelector('#monthly-payment');
+  monthly = monthly.toString();
+  paymentSpan.textContent = monthly;
 }
