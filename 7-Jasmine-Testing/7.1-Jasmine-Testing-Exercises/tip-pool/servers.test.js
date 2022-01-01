132701f570e8e1bf2e8a57a257164e12b88f7e04
James Hudnall <jimmyhudnall@gmail.com>
2022-01-01 09:01:04 -0500

    Implement tests for servers.test

--- 7-Jasmine-Testing/7.1-Jasmine-Testing-Exercises/tip-pool/servers.test.js
+++ 7-Jasmine-Testing/7.1-Jasmine-Testing-Exercises/tip-pool/servers.test.js
@@ -2,6 +2,8 @@ describe("Servers test (with setup and tear-down)", function() {
   beforeEach(function () {
     // initialization logic
     serverNameInput.value = 'Alice';
+    // billAmtInput.value = 100;
+    // tipAmtInput.value=10;
   });
 
   it('should add a new server to allServers on submitServerInfo()', function () {
@@ -11,7 +13,22 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
+  it('should update serverTable', function() {
+    // Implement Tests for updateServerTable();
+    submitServerInfo();
+
+    let cells = document.querySelectorAll('#serverTable tbody tr td');
+
+    expect(cells.length).toEqual(2);
+    expect(cells[0].innerText).toBe('Alice');
+    expect(cells[1].innerText).toEqual('$0.00');
+  });
+
   afterEach(function() {
     // teardown logic
+    document.querySelector('#serverTable tbody').innerHTML = '';
+    serverNameInput.value = '';
+    serverId = 0;
+    allServers = {};
   });
 });
