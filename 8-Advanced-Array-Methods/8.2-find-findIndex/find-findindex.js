044aaef6cd5ff817659dd78945c667d4cb8cc95e
James Hudnall <jimmyhudnall@gmail.com>
2022-01-02 07:40:25 -0500

    Implement all solutions

--- 8-Advanced-Array-Methods/8.2-find-findIndex/find-findindex.js
+++ 8-Advanced-Array-Methods/8.2-find-findIndex/find-findindex.js
@@ -12,7 +12,11 @@ findUserByUsername(users, 'taco') // undefined
 */
 
 
-function findUserByUsername(usersArray, username) {}
+function findUserByUsername(usersArray, username) {
+  return usersArray.find(function(obj) {
+    return obj.username === username;
+  })
+}
 
 /*
 Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined.
@@ -27,4 +31,14 @@ removeUser(users, 'akagen') // {username: 'akagen'}
 removeUser(users, 'akagen') // undefined
 */
 
-function removeUser(usersArray, username) {}
+function removeUser(usersArray, username) {
+  // I: array, string
+  const targetIdx = usersArray.findIndex(function(obj) {
+    return obj.username === username;
+  });
+  if (targetIdx !== -1) {
+    const user = usersArray[targetIdx];
+    usersArray.splice(targetIdx, 1);
+    return user;
+  }
+}
