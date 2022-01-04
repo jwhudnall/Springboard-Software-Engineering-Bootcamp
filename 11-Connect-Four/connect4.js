9fdc85c0008ea1857d109e5b955a14c132ccd309
James Hudnall <jimmyhudnall@gmail.com>
2022-01-04 10:32:36 -0500

    Implement initial solution.
    All 8 required steps completed.

--- 11-Connect-Four/connect4.js
+++ 11-Connect-Four/connect4.js
@@ -5,11 +5,11 @@
  * board fills (tie)
  */
 
-var WIDTH = 7;
-var HEIGHT = 6;
+const WIDTH = 7;
+const HEIGHT = 6;
 
-var currPlayer = 1; // active player: 1 or 2
-var board = []; // array of rows, each row is array of cells  (board[y][x])
+let currPlayer = 1; // active player: 1 or 2
+const board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
@@ -17,34 +17,44 @@ var board = []; // array of rows, each row is array of cells  (board[y][x])
 
 function makeBoard() {
   // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+  for (let i = 0; i < HEIGHT; i++) {
+    let row = [];
+    for (let j = 0; j < WIDTH; j++) {
+      row[j] = null;
+    }
+    board.push(row);
+  }
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
 function makeHtmlBoard() {
   // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
+  const htmlBoard = document.querySelector('#board');
 
   // TODO: add comment for this code
-  var top = document.createElement("tr");
-  top.setAttribute("id", "column-top");
-  top.addEventListener("click", handleClick);
+  const top = document.createElement("tr");  // Create a table row element
+  top.setAttribute("id", "column-top"); // add #column-top to new row element
+  top.addEventListener("click", handleClick); // Add click listener to row
 
+  // Populate the top row by adding the number of values in WIDTH, adding unique IDs to each cell
   for (var x = 0; x < WIDTH; x++) {
     var headCell = document.createElement("td");
     headCell.setAttribute("id", x);
     top.append(headCell);
   }
-  htmlBoard.append(top);
+  htmlBoard.append(top);  // append top row to board
 
   // TODO: add comment for this code
+  // Similar to makeBoard(), populate the game board with <td>'s using a nested loop
   for (var y = 0; y < HEIGHT; y++) {
     const row = document.createElement("tr");
     for (var x = 0; x < WIDTH; x++) {
       const cell = document.createElement("td");
-      cell.setAttribute("id", `${y}-${x}`);
-      row.append(cell);
+      cell.setAttribute("id", `${x}-${y}`); // Add unique ID based on cell position
+      row.append(cell); // Add each cell to current row
     }
-    htmlBoard.append(row);
+    htmlBoard.append(row); // Add each row to board
   }
 }
 
@@ -52,19 +62,33 @@ function makeHtmlBoard() {
 
 function findSpotForCol(x) {
   // TODO: write the real version of this, rather than always returning 0
-  return 0;
+  for (let i = HEIGHT - 1; i >= 0; i--) {
+    const row = board[i];
+    if (row[x] === null) {
+      return i;
+    }
+  }
+  return null;
 }
 
 /** placeInTable: update DOM to place piece into HTML table of board */
 
 function placeInTable(y, x) {
   // TODO: make a div and insert into correct table cell
+  const piece = document.createElement('div');
+  // Create new DIV element
+  piece.classList.add('piece', `p${currPlayer}`);
+  // Add class piece, as well as p1 or p2 (check current);
+  // Append to correct cell
+  document.getElementById(`${x}-${y}`).append(piece);
+
 }
 
 /** endGame: announce game end */
 
 function endGame(msg) {
   // TODO: pop up alert message
+  alert(msg);
 }
 
 /** handleClick: handle click of column top to play piece */
@@ -81,7 +105,9 @@ function handleClick(evt) {
 
   // place piece in board and add to HTML table
   // TODO: add line to update in-memory board
+  board[y][x] = currPlayer;
   placeInTable(y, x);
+
 
   // check for win
   if (checkForWin()) {
@@ -90,9 +116,13 @@ function handleClick(evt) {
 
   // check for tie
   // TODO: check if all cells in board are filled; if so call, call endGame
-
+  const boardIsFull = board.flat().every(val => val !== null);
+  if (boardIsFull) {
+    endGame();
+  }
   // switch players
   // TODO: switch currPlayer 1 <-> 2
+  currPlayer = currPlayer === 1 ? 2 : 1;
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
