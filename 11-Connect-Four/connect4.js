20b232cf8e8d8f2f4c4e166d3b71b427f4f3ae79
James Hudnall <jimmyhudnall@gmail.com>
2022-01-04 19:18:03 -0500

    Clean up comments

--- 11-Connect-Four/connect4.js
+++ 11-Connect-Four/connect4.js
@@ -7,65 +7,59 @@
 
 const WIDTH = 7;
 const HEIGHT = 6;
-
-let currPlayer = 1; // active player: 1 or 2
+let currPlayer = 1;
 const board = []; // array of rows, each row is array of cells  (board[y][x])
+let gameIsActive = true;
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
  */
 
 function makeBoard() {
-  // TODO: set "board" to empty HEIGHT x WIDTH matrix array
-  for (let i = 0; i < HEIGHT; i++) {
+  for (let y = 0; y < HEIGHT; y++) {
     let row = [];
-    for (let j = 0; j < WIDTH; j++) {
-      row[j] = null;
+    for (let x = 0; x < WIDTH; x++) {
+      row[x] = null;
     }
     board.push(row);
   }
 }
 
-/** makeHtmlBoard: make HTML table and row of column tops. */
-
 function makeHtmlBoard() {
-  // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
   const htmlBoard = document.querySelector('#board');
 
-  // TODO: add comment for this code
-  const top = document.createElement("tr");  // Create a table row element
-  top.setAttribute("id", "column-top"); // add #column-top to new row element
-  top.addEventListener("click", handleClick); // Add click listener to row
+  // Creates and populates top row of gameboard, including a click listener
+  const top = document.createElement("tr");
+  top.setAttribute("id", "column-top");
+  top.addEventListener("click", handleClick);
 
-  // Populate the top row by adding the number of values in WIDTH, adding unique IDs to each cell
-  for (var x = 0; x < WIDTH; x++) {
-    var headCell = document.createElement("td");
+  // Populate the remaining rows by WIDTH, HEIGHT, each with unique ID
+  for (let x = 0; x < WIDTH; x++) {
+    const headCell = document.createElement("td");
     headCell.setAttribute("id", x);
     top.append(headCell);
   }
-  htmlBoard.append(top);  // append top row to board
+  htmlBoard.append(top);
 
-  // TODO: add comment for this code
-  // Similar to makeBoard(), populate the game board with <td>'s using a nested loop
-  for (var y = 0; y < HEIGHT; y++) {
+  // Populate the HTML game board
+  for (let y = 0; y < HEIGHT; y++) {
     const row = document.createElement("tr");
-    for (var x = 0; x < WIDTH; x++) {
+    for (let x = 0; x < WIDTH; x++) {
       const cell = document.createElement("td");
-      cell.setAttribute("id", `${x}-${y}`); // Add unique ID based on cell position
-      row.append(cell); // Add each cell to current row
+      cell.setAttribute("id", `${x}-${y}`);
+      row.append(cell);
     }
-    htmlBoard.append(row); // Add each row to board
+    htmlBoard.append(row);
   }
 }
 
 /** findSpotForCol: given column x, return top empty y (null if filled) */
 
 function findSpotForCol(x) {
-  // TODO: write the real version of this, rather than always returning 0
-  for (let i = HEIGHT - 1; i >= 0; i--) {
-    const row = board[i];
-    if (row[x] === null) {
-      return i;
+  for (let y = HEIGHT - 1; y >= 0; y--) {
+    const row = board[y];
+    if (!row[x]) {
+      return y;
     }
   }
   return null;
@@ -74,54 +68,44 @@ function findSpotForCol(x) {
 /** placeInTable: update DOM to place piece into HTML table of board */
 
 function placeInTable(y, x) {
-  // TODO: make a div and insert into correct table cell
   const piece = document.createElement('div');
-  piece.classList.add('piece', `p${currPlayer}`);
-  piece.classList.add('fall'); // Animation
-  // Append to correct cell
+  piece.classList.add('piece', `p${currPlayer}`, 'fall'); // .fall = animation
   document.getElementById(`${x}-${y}`).append(piece);
-
 }
 
 /** endGame: announce game end */
 
 function endGame(msg) {
-  // TODO: pop up alert message
-  alert(msg);
+  msg ? alert(msg) : alert('Tie Game!');
+  gameIsActive = false;
 }
 
 /** handleClick: handle click of column top to play piece */
 
 function handleClick(evt) {
-  // get x from ID of clicked cell
-  var x = +evt.target.id;
+  if (gameIsActive) {
+    const x = +evt.target.id;
+    const y = findSpotForCol(x);
+    if (y === null) {
+      return;
+    }
 
-  // get next spot in column (if none, ignore click)
-  var y = findSpotForCol(x);
-  if (y === null) {
-    return;
+    // place piece in board and add to HTML table
+    placeInTable(y, x);
+    board[y][x] = currPlayer;
+
+    if (checkForWin()) {
+      return endGame(`Player ${currPlayer} won!`);
+    }
+
+    // check for tie
+    const boardIsFull = board.flat().every(val => val !== null);
+    if (boardIsFull) {
+      endGame();
+    }
+    // switch players
+    currPlayer = currPlayer === 1 ? 2 : 1;
   }
-
-  // place piece in board and add to HTML table
-  // TODO: add line to update in-memory board
-  board[y][x] = currPlayer;
-  placeInTable(y, x);
-
-
-  // check for win
-  if (checkForWin()) {
-    return endGame(`Player ${currPlayer} won!`);
-  }
-
-  // check for tie
-  // TODO: check if all cells in board are filled; if so call, call endGame
-  const boardIsFull = board.flat().every(val => val !== null);
-  if (boardIsFull) {
-    endGame();
-  }
-  // switch players
-  // TODO: switch currPlayer 1 <-> 2
-  currPlayer = currPlayer === 1 ? 2 : 1;
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
@@ -144,12 +128,12 @@ function checkForWin() {
 
   // TODO: read and understand this code. Add comments to help you.
 
-  for (var y = 0; y < HEIGHT; y++) {  // row representation, from 0 to height - 1
-    for (var x = 0; x < WIDTH; x++) { // col rep, from 0 to width - 1
-      var horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]]; // Looks for 4 consecutive (horiz)
-      var vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]]; // Looks for 4 consecutive (vert)
-      var diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]]; // 4 consec Diag / Down-Right
-      var diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];// 4 consec Diag / Down-Left
+  for (let y = 0; y < HEIGHT; y++) {  // row representation, from 0 to height - 1
+    for (let x = 0; x < WIDTH; x++) { // col rep, from 0 to width - 1
+      let horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]]; // Looks for 4 consecutive (horiz)
+      let vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]]; // Looks for 4 consecutive (vert)
+      let diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]]; // 4 consec Diag / Down-Right
+      let diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];// 4 consec Diag / Down-Left
 
       // If any of the 4 checks above returns true, a match is found, true is returned
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
