7427bb7fa7dd308fcf69ee4984d30d37372694d2
James Hudnall <jimmyhudnall@gmail.com>
2022-01-07 13:24:30 -0500

    Implement final solution

--- 14-Web-101/14.3-Giphy-Party/app.js
+++ 14-Web-101/14.3-Giphy-Party/app.js
@@ -1,25 +1,39 @@
+const gifContainer = document.querySelector('#gif-container');
 const gifForm = document.querySelector('#gif-form');
+const delBtn = document.querySelector('#remove-btn');
+
+delBtn.addEventListener('click',clearGifs);
 
 gifForm.addEventListener('submit', function (e) {
   e.preventDefault();
   const q = e.target.search.value;
   giphySearch(q);
+  e.target.search.value = '';
 })
 
+
 const giphySearch = async function (q) {
-  const res = await axios.get('https://api.giphy.com/v1/gifs/search', {
-    params: {
-      api_key: 'X8u8SGkuk7axOu2S1PpGLm5DJPKYthKq',
-      q:q
-    }
-  });
-  appendGif(res);
+  try {
+    const res = await axios.get('https://api.giphy.com/v1/gifs/search', {
+      params: {
+        api_key: 'X8u8SGkuk7axOu2S1PpGLm5DJPKYthKq',
+        q
+      }
+    });
+    appendGif(res);
+  } catch (e) {
+    alert('Try another search term!');
+  }
 }
 
 const appendGif = function (res) {
-  // TODO: Randomize returned image
-  const gifURL = res.data.data[0].images.original.url;
+  const randIdx = Math.floor(Math.random() * res.data.data.length);
+  const gifURL = res.data.data[randIdx].images.original.url;
   const newGif = document.createElement('img');
   newGif.src = gifURL;
-  document.body.append(newGif);
+  gifContainer.append(newGif);
+}
+
+function clearGifs() {
+  gifContainer.innerHTML = '';
 }
