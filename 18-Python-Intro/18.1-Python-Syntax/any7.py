6ad994d471efce225e3d47194d421f255b3f77e5
James Hudnall <jimmyhudnall@gmail.com>
2022-01-12 09:16:08 -0500

    Complete any7, convert exercises

--- 18-Python-Intro/18.1-Python-Syntax/any7.py
+++ 18-Python-Intro/18.1-Python-Syntax/any7.py
@@ -1,7 +1,11 @@
 def any7(nums):
     """Are any of these numbers a 7? (True/False)"""
+    for num in nums:
+        if num == 7:
+            return True
+    return False
 
-    # YOUR CODE HERE 
+    # YOUR CODE HERE
 
 print("should be true", any7([1, 2, 7, 4, 5]))
 print("should be false", any7([1, 2, 4, 5]))
--- 18-Python-Intro/18.1-Python-Syntax/convert.py
+++ 18-Python-Intro/18.1-Python-Syntax/convert.py
@@ -1,7 +1,7 @@
 def convert_temp(unit_in, unit_out, temp):
     """Convert farenheit <-> celsius and return results.
 
-    - unit_in: either "f" or "c" 
+    - unit_in: either "f" or "c"
     - unit_out: either "f" or "c"
     - temp: temperature (in f or c, depending on unit_in)
 
@@ -16,6 +16,22 @@ def convert_temp(unit_in, unit_out, temp):
     """
 
     # YOUR CODE HERE
+    valid_unit_in = unit_in == 'f' or unit_in == 'c'
+    valid_unit_out = unit_out == 'f' or unit_out == 'c'
+
+    if valid_unit_in and valid_unit_out and unit_in == 'f':
+        if unit_out == 'f':
+            return temp
+        else:
+            return (temp - 32) / 1.8 # f to c conversion
+    elif valid_unit_in and valid_unit_out: # c first
+        if unit_out == 'c':
+            return temp;
+        else:
+            return temp * 1.8 + 32 # c to f conversion
+    else:
+        invalid_val = unit_in if not valid_unit_in else unit_out
+        return f'Invalid unit: {invalid_val}'
 
 
 print("c", "f", 0, convert_temp("c", "f", 0), "should be 32.0")
@@ -23,4 +39,3 @@ print("f", "c", 212, convert_temp("f", "c", 212), "should be 100.0")
 print("z", "f", 32, convert_temp("z", "f", 32), "should be Invalid unit z")
 print("c", "z", 32, convert_temp("c", "z", 32), "should be Invalid unit z")
 print("f", "f", 75.5, convert_temp("f", "f", 75.5), "should be 75.5")
-
