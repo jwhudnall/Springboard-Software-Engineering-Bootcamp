5a6637cb765567df0e2929ebfde03e4ca9783c88
James Hudnall <jimmyhudnall@gmail.com>
2022-01-13 08:23:49 -0500

    Add solution to weekday_name

--- 18-Python-Intro/18.2-Python-Data-Structures/02_weekday_name/weekday_name.py
+++ 18-Python-Intro/18.2-Python-Data-Structures/02_weekday_name/weekday_name.py
@@ -1,14 +1,18 @@
 def weekday_name(day_of_week):
     """Return name of weekday.
-    
+
         >>> weekday_name(1)
         'Sunday'
-        
+
         >>> weekday_name(7)
         'Saturday'
-        
+
     For days not between 1 and 7, return None
-    
+
         >>> weekday_name(9)
         >>> weekday_name(0)
     """
+    days = ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday')
+    day_idx = day_of_week - 1
+
+    return days[day_idx] if day_idx >= 0 and day_idx <= 7 else None
