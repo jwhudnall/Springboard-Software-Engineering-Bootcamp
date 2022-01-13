41473915c822d952b39f4371b1bd3c665f3272b5
James Hudnall <jimmyhudnall@gmail.com>
2022-01-13 08:24:51 -0500

    Implement last_element

--- 18-Python-Intro/18.2-Python-Data-Structures/03_last_element/last_element.py
+++ 18-Python-Intro/18.2-Python-Data-Structures/03_last_element/last_element.py
@@ -1,9 +1,10 @@
 def last_element(lst):
     """Return last item in list (None if list is empty.
-    
+
         >>> last_element([1, 2, 3])
         3
-        
+
         >>> last_element([]) is None
         True
     """
+    return lst[-1] if lst else None
