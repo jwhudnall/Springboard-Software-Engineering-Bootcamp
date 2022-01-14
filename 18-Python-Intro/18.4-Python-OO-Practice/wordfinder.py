a36161f2a21c360287950207c7b52f8c64fddea4
James Hudnall <jimmyhudnall@gmail.com>
2022-01-14 13:18:03 -0500

    Implement WordFinder class

--- 18-Python-Intro/18.4-Python-OO-Practice/wordfinder.py
+++ 18-Python-Intro/18.4-Python-OO-Practice/wordfinder.py
@@ -2,4 +2,37 @@
 
 
 class WordFinder:
-    ...
+    """Class info will go here
+
+    >>> wf = WordFinder('words.txt')
+    235886 words read
+
+    >>> wf.word_list_len
+    235886
+
+    >>> wf.word_list[:5]
+    ['A', 'a', 'aa', 'aal', 'aalii']
+
+    """
+
+    def __init__(self, dir):
+        """Initializes the WordFinder Class"""
+        self.dir = dir
+        self.word_list = self.get_words()
+        self.word_list_len = len(self.word_list)
+        self.print_word_count()
+
+    def print_word_count(self):
+        """Prints the number of words read"""
+        print(f'{self.word_list_len} words read')
+
+    def get_words(self):
+        """Returns the list of lines extracted from the file specified in the dir argument"""
+        with open(self.dir) as file:
+            words = [line.split('\n')[0] for line in file]
+        return words
+
+    def random(self):
+        """Extract a random word from the word_list"""
+        from random import choice
+        return choice(self.word_list)
