4c12a9a11598785aa52a013337a300f25d2446d8
James Hudnall <jimmyhudnall@gmail.com>
2022-01-15 15:46:08 -0500

    Implement solution to all work

--- 19-Flask-Intro/19.1-Flask-Greet-Calc/calc/app.py
+++ 19-Flask-Intro/19.1-Flask-Greet-Calc/calc/app.py
@@ -1,1 +1,51 @@
-# Put your app in here.
+from flask import Flask, request
+from operations import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+  return "Welcome to Calc"
+
+@app.route('/math/<operation>')
+def handle_math(operation):
+  operations = {
+    'add': add,
+    'sub': sub,
+    'mult': mult,
+    'div' :div
+  }
+
+  a = int(request.args.get('a'))
+  b = int(request.args.get('b'))
+  res = operations[operation](a, b)
+  return str(res)
+
+
+# @app.route('/add')
+# def add_val():
+#   a = request.args['a']
+#   b = request.args['b']
+#   res = add(int(a), int(b))
+#   return str(res)
+
+# @app.route('/sub')
+# def sub_val():
+#   a = request.args['a']
+#   b = request.args['b']
+#   res = sub(int(a), int(b))
+#   return str(res)
+
+# @app.route('/mult')
+# def mult_val():
+#   a = request.args['a']
+#   b = request.args['b']
+#   res = mult(int(a), int(b))
+#   return str(res)
+
+# @app.route('/div')
+# def div_val():
+#   a = request.args['a']
+#   b = request.args['b']
+#   res = div(int(a), int(b))
+#   return str(res)
