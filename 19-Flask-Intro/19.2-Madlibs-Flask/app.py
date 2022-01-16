1d634aad4a5cf50cea6b2405cfc6041ca47993d9
James Hudnall <jimmyhudnall@gmail.com>
2022-01-16 16:01:19 -0500

    Implement dynamic story list

--- 19-Flask-Intro/19.2-Madlibs-Flask/app.py
+++ 19-Flask-Intro/19.2-Madlibs-Flask/app.py
@@ -1,17 +1,26 @@
 from flask import Flask, render_template, request
-from stories import story
+from stories import stories
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def madlib_form():
-    words = story.prompts
-    return render_template('/madlibs.html', words=words)
+def display_homepage():
+    return render_template('/stories.html', story_list = stories.values())
 
+@app.route('/questions')
+def madlib_form():
+    story_id = request.args['story_id']
+    print(f'story_id: {story_id}')
+    story = stories[story_id]
+    prompts = story.prompts
+
+    return render_template('/madlibs.html', story_id=story_id, title=story.title, words=prompts)
 
 @app.route('/story')
 def show_story():
+    story_id = request.args['story_id']
+    story = stories[story_id]
     words = request.args
     user_story = story.generate(words)
     return render_template('/story.html', story=user_story)
--- 19-Flask-Intro/19.2-Madlibs-Flask/stories.py
+++ 19-Flask-Intro/19.2-Madlibs-Flask/stories.py
@@ -18,9 +18,10 @@ class Story:
         'I love to eat a good mango.'
     """
 
-    def __init__(self, words, text):
+    def __init__(self, code, title, words, text):
         """Create story with words and template text."""
-
+        self.code = code
+        self.title = title
         self.prompts = words
         self.template = text
 
@@ -38,8 +39,19 @@ class Story:
 # Here's a story to get you started
 
 
-story = Story(
+story1 = Story(
+    'long-ago',
+    'Once Upon a Time',
     ["place", "noun", "verb", "adjective", "plural_noun"],
     """Once upon a time in a long-ago {place}, there lived a
        large {adjective} {noun}. It loved to {verb} {plural_noun}."""
 )
+
+story2 = Story(
+    'frantic',
+    'The Crazy House',
+    ['adjective_1', 'adjective_2','type_of_bird', 'room_in_a_house', 'verb_past_tense', 'verb', 'relatives_name', 'noun', 'liquid', 'verb_ends_with_ing', 'part_of_body_plural', 'plural_noun'],
+    """It was a {adjective_1}, cold November day. I woke up to the {adjective_2} smell of {type_of_bird} roasting in the {room_in_a_house} downstairs. I {verb_past_tense} down the stairs to see if I could help {verb} the dinner. My mom said, \"See if {relatives_name} needs a fresh {noun}.\" So i carried a tray of glasses full of {liquid} into the {verb_ends_with_ing} room. When I got there, I couldn't believe my {part_of_body_plural}! There were {plural_noun} {verb_ends_with_ing} on the {noun}!"""
+)
+
+stories = {s.code: s for s in [story1, story2]}
