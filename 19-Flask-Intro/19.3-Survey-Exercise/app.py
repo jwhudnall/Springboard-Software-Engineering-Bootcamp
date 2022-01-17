358e9b613f426ddae6389edafc7576fbcc8724a9
James Hudnall <jimmyhudnall@gmail.com>
2022-01-17 13:38:33 -0500

    Implement step 6

--- 19-Flask-Intro/19.3-Survey-Exercise/app.py
+++ 19-Flask-Intro/19.3-Survey-Exercise/app.py
@@ -8,30 +8,42 @@ app.config['SECRET_KEY'] = 'not-very-secret'
 debug = DebugToolbarExtension(app)
 
 responses = []
+current_question = 1
+
 
 @app.route('/')
 def show_homepage():
     return render_template('/survey.html', survey=satisfaction_survey)
 
+
 @app.route('/questions/<int:question_num>')
 def render_question(question_num):
-    question = satisfaction_survey.questions[question_num]
-    return render_template('/question.html', question=question, num=question_num)
 
-@app.route('/answer', methods=["POST"])
-def handle_answer():
-    # extract and handle form answer request.form
-    # q0-ans AND q-num returns submitted question number (0)
-    question_num = int(request.form['q-num'])
-    question_ans = request.form[f'q{question_num}-ans']
-    responses.append(question_ans)
-    next_q_idx = question_num + 1
-
-    if next_q_idx < len(satisfaction_survey.questions):
-        return redirect(url_for('render_question',question_num=next_q_idx))
-    else:
+    if (len(responses) == len(satisfaction_survey.questions)):
         return redirect('/thanks')
 
-@app.route('/thanks')
+    if len(responses) == question_num :
+        question = satisfaction_survey.questions[question_num]
+        return render_template('/question.html', question=question, num=question_num)
+
+
+    return redirect(url_for('render_question', question_num=len(responses)))
+
+
+@ app.route('/answer', methods=["POST"])
+def handle_answer():
+    # extract and handle form answer request.form
+    question_num = int(request.form['q-num'])  # 1
+    question_ans = request.form[f'q{question_num}-ans']
+    responses.append(question_ans)
+
+    if (len(responses) == len(satisfaction_survey.questions)):
+        return redirect('/thanks')
+
+    else:
+        return redirect(url_for('render_question', question_num=len(responses)))
+
+
+@ app.route('/thanks')
 def render_survey_complete():
     return render_template('/thanks.html')
