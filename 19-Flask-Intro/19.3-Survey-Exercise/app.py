749d68b7f255929ead6803fc73e15ed4dd10587e
James Hudnall <jimmyhudnall@gmail.com>
2022-01-17 11:32:52 -0500

    Implement step 5

--- 19-Flask-Intro/19.3-Survey-Exercise/app.py
+++ 19-Flask-Intro/19.3-Survey-Exercise/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -12,3 +12,26 @@ responses = []
 @app.route('/')
 def show_homepage():
     return render_template('/survey.html', survey=satisfaction_survey)
+
+@app.route('/questions/<int:question_num>')
+def render_question(question_num):
+    question = satisfaction_survey.questions[question_num]
+    return render_template('/question.html', question=question, num=question_num)
+
+@app.route('/answer', methods=["POST"])
+def handle_answer():
+    # extract and handle form answer request.form
+    # q0-ans AND q-num returns submitted question number (0)
+    question_num = int(request.form['q-num'])
+    question_ans = request.form[f'q{question_num}-ans']
+    responses.append(question_ans)
+    next_q_idx = question_num + 1
+
+    if next_q_idx < len(satisfaction_survey.questions):
+        return redirect(url_for('render_question',question_num=next_q_idx))
+    else:
+        return redirect('/thanks')
+
+@app.route('/thanks')
+def render_survey_complete():
+    return render_template('/thanks.html')
