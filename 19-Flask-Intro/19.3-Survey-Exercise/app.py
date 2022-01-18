ba1acce4499db9eadcd3d04a499112465a7d3a2b
James Hudnall <jimmyhudnall@gmail.com>
2022-01-18 08:47:02 -0500

    Implement Step 8

--- 19-Flask-Intro/19.3-Survey-Exercise/app.py
+++ 19-Flask-Intro/19.3-Survey-Exercise/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for, flash
+from flask import Flask, request, render_template, redirect, url_for, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -8,30 +8,26 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-responses = []
-current_question = 1
-
 
 @app.route('/')
 def show_homepage():
+    session['responses'] = []
     return render_template('/survey.html', survey=satisfaction_survey)
 
 
-@app.route('/questions/<int:question_num>')
+@app.route('/questions/<int:question_num>', methods=['GET', 'POST'])
 def render_question(question_num):
+    responses = session['responses']
 
     if (len(responses) == len(satisfaction_survey.questions)):
         return redirect('/thanks')
 
     if len(responses) != question_num :
-        # flash('Please use the form to answer questions in order')
         flash('Please use the form to submit answers.')
-        print('User flashed! AHH')
         return redirect(url_for('render_question', question_num=len(responses)))
 
     question = satisfaction_survey.questions[question_num]
     return render_template('/question.html', question=question, num=question_num)
-
 
 
 
@@ -40,7 +36,10 @@ def handle_answer():
     # extract and handle form answer request.form
     question_num = int(request.form['q-num'])  # 1
     question_ans = request.form[f'q{question_num}-ans']
+
+    responses = session['responses']
     responses.append(question_ans)
+    session['responses'] = responses
 
     if (len(responses) == len(satisfaction_survey.questions)):
         return redirect('/thanks')
