5c777b5210fc2ee73d1fcfd1f95c12aa7933df2e
James Hudnall <jimmyhudnall@gmail.com>
2022-01-18 10:03:45 -0500

    Implement FS One and Two

--- 19-Flask-Intro/19.3-Survey-Exercise/app.py
+++ 19-Flask-Intro/19.3-Survey-Exercise/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, redirect, url_for, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-from surveys import satisfaction_survey
+from surveys import surveys
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'not-very-secret'
@@ -11,22 +11,42 @@ debug = DebugToolbarExtension(app)
 
 @app.route('/')
 def show_homepage():
+    # list available surveys
+    return render_template('/survey-form.html', surveys=surveys)
+
+@app.route('/start-survey', methods=['POST'])
+def handle_form():
+    # redirect to specific survey
+    survey_name = request.form['survey-name']
+    session['survey_name'] = survey_name
+    # session['survey_obj'] = surveys[survey_name]
+    return redirect(f'/surveys/{survey_name}')
+    # check if name exists as a survey? if not, redirect? Shouldn't need
+    # due to list selection?
+
+@app.route('/surveys/<survey_name>')
+def survey_start(survey_name):
+    # add session cookie for survey key
     session['responses'] = []
-    return render_template('/survey.html', survey=satisfaction_survey)
+    survey = surveys[survey_name]
+    return render_template('/survey.html', survey=survey)
 
 
 @app.route('/questions/<int:question_num>', methods=['GET', 'POST'])
 def render_question(question_num):
     responses = session['responses']
+    # survey = session['survey_obj']
+    survey_name = session['survey_name']
+    survey = surveys[survey_name]
 
-    if (len(responses) == len(satisfaction_survey.questions)):
+    if (len(responses) == len(survey.questions)):
         return redirect('/thanks')
 
     if len(responses) != question_num :
         flash('Please use the form to submit answers.')
         return redirect(url_for('render_question', question_num=len(responses)))
 
-    question = satisfaction_survey.questions[question_num]
+    question = survey.questions[question_num]
     return render_template('/question.html', question=question, num=question_num)
 
 
@@ -36,12 +56,14 @@ def handle_answer():
     # extract and handle form answer request.form
     question_num = int(request.form['q-num'])  # 1
     question_ans = request.form[f'q{question_num}-ans']
+    survey_name = session['survey_name']
+    survey = surveys[survey_name]
 
     responses = session['responses']
     responses.append(question_ans)
     session['responses'] = responses
 
-    if (len(responses) == len(satisfaction_survey.questions)):
+    if (len(responses) == len(survey.questions)):
         return redirect('/thanks')
 
     else:
