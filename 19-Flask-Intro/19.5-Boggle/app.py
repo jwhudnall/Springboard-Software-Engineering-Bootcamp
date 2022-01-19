f7f00ef4675c2b846afe17d3065299a7bc59d8db
James Hudnall <jimmyhudnall@gmail.com>
2022-01-19 08:34:59 -0500

    Implement Steps One and Two

--- 19-Flask-Intro/19.5-Boggle/app.py
+++ 19-Flask-Intro/19.5-Boggle/app.py
@@ -1,3 +1,18 @@
 from boggle import Boggle
+from flask import Flask, request, render_template, session
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'easy-as-123'
+debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
+
+@app.route('/')
+def show_board():
+    # send board to template
+    game = Boggle()
+    board = Boggle.make_board(game)
+    session['board'] = board
+
+    return render_template('gameboard.html', board=board)
