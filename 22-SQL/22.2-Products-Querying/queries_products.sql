49b24e338e48481641bdfcae99b603193a0a9480
James Hudnall <jimmyhudnall@gmail.com>
2022-01-23 11:34:11 -0500

    Implement Part 4

--- 22-SQL/22.2-Products-Querying/queries_products.sql
+++ 22-SQL/22.2-Products-Querying/queries_products.sql
@@ -11,6 +11,7 @@ INSERT INTO products (name, price, can_be_returned)
 -- 3. Add a product to the table with the name of “table”, price of 124.00, and can_be_returned of false.
 INSERT INTO products (name, price, can_be_returned)
   VALUES ('table', 124.00, false);
+
 -- 4. Display all of the rows and columns in the table.
 SELECT * FROM products;
 
