4a2940d0f8770086e2b20c332debfc8bab7350b8
James Hudnall <jimmyhudnall@gmail.com>
2022-01-25 11:48:30 -0500

    Add music diagram and refactor code

--- 22-SQL/22.4-Data-Modeling-Exercise/music.sql
+++ 22-SQL/22.4-Data-Modeling-Exercise/music.sql
@@ -7,27 +7,81 @@ CREATE DATABASE music;
 
 \c music
 
+-- CREATE TABLE songs
+-- (
+--   id SERIAL PRIMARY KEY,
+--   title TEXT NOT NULL,
+--   duration_in_seconds INTEGER NOT NULL,
+--   release_date DATE NOT NULL,
+--   artists TEXT[] NOT NULL,
+--   album TEXT NOT NULL,
+--   producers TEXT[] NOT NULL
+-- );
+
+CREATE TABLE artists
+(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
 CREATE TABLE songs
 (
   id SERIAL PRIMARY KEY,
-  title TEXT NOT NULL,
-  duration_in_seconds INTEGER NOT NULL,
-  release_date DATE NOT NULL,
-  artists TEXT[] NOT NULL,
-  album TEXT NOT NULL,
-  producers TEXT[] NOT NULL
+  title VARCHAR(30) NOT NULL,
+  duration_in_seconds INT NOT NULL,
+  release_date DATE,
+  album VARCHAR(30)
 );
 
-INSERT INTO songs
-  (title, duration_in_seconds, release_date, artists, album, producers)
+CREATE TABLE producers
+(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE songs_artists_producers
+(
+  id SERIAL PRIMARY KEY,
+  songid INT REFERENCES songs ON DELETE SET NULL,
+  artistid INT REFERENCES artists ON DELETE SET NULL,
+  producerid INT REFERENCES producers ON DELETE SET NULL
+);
+
+-- Insertion
+
+-- INSERT INTO songs
+--   (title, duration_in_seconds, release_date, artists, album, producers)
+-- VALUES
+--   ('MMMBop', 238, '04-15-1997', '{"Hanson"}', 'Middle of Nowhere', '{"Dust Brothers", "Stephen Lironi"}'),
+--   ('Bohemian Rhapsody', 355, '10-31-1975', '{"Queen"}', 'A Night at the Opera', '{"Roy Thomas Baker"}'),
+--   ('One Sweet Day', 282, '11-14-1995', '{"Mariah Cary", "Boyz II Men"}', 'Daydream', '{"Walter Afanasieff"}'),
+--   ('Shallow', 216, '09-27-2018', '{"Lady Gaga", "Bradley Cooper"}', 'A Star Is Born', '{"Benjamin Rice"}'),
+--   ('How You Remind Me', 223, '08-21-2001', '{"Nickelback"}', 'Silver Side Up', '{"Rick Parashar"}'),
+--   ('New York State of Mind', 276, '10-20-2009', '{"Jay Z", "Alicia Keys"}', 'The Blueprint 3', '{"Al Shux"}'),
+--   ('Dark Horse', 215, '12-17-2013', '{"Katy Perry", "Juicy J"}', 'Prism', '{"Max Martin", "Cirkut"}'),
+--   ('Moves Like Jagger', 201, '06-21-2011', '{"Maroon 5", "Christina Aguilera"}', 'Hands All Over', '{"Shellback", "Benny Blanco"}'),
+--   ('Complicated', 244, '05-14-2002', '{"Avril Lavigne"}', 'Let Go', '{"The Matrix"}'),
+--   ('Say My Name', 240, '11-07-1999', '{"Destiny''s Child"}', 'The Writing''s on the Wall', '{"Darkchild"}');
+
+INSERT INTO artists
+(name)
 VALUES
-  ('MMMBop', 238, '04-15-1997', '{"Hanson"}', 'Middle of Nowhere', '{"Dust Brothers", "Stephen Lironi"}'),
-  ('Bohemian Rhapsody', 355, '10-31-1975', '{"Queen"}', 'A Night at the Opera', '{"Roy Thomas Baker"}'),
-  ('One Sweet Day', 282, '11-14-1995', '{"Mariah Cary", "Boyz II Men"}', 'Daydream', '{"Walter Afanasieff"}'),
-  ('Shallow', 216, '09-27-2018', '{"Lady Gaga", "Bradley Cooper"}', 'A Star Is Born', '{"Benjamin Rice"}'),
-  ('How You Remind Me', 223, '08-21-2001', '{"Nickelback"}', 'Silver Side Up', '{"Rick Parashar"}'),
-  ('New York State of Mind', 276, '10-20-2009', '{"Jay Z", "Alicia Keys"}', 'The Blueprint 3', '{"Al Shux"}'),
-  ('Dark Horse', 215, '12-17-2013', '{"Katy Perry", "Juicy J"}', 'Prism', '{"Max Martin", "Cirkut"}'),
-  ('Moves Like Jagger', 201, '06-21-2011', '{"Maroon 5", "Christina Aguilera"}', 'Hands All Over', '{"Shellback", "Benny Blanco"}'),
-  ('Complicated', 244, '05-14-2002', '{"Avril Lavigne"}', 'Let Go', '{"The Matrix"}'),
-  ('Say My Name', 240, '11-07-1999', '{"Destiny''s Child"}', 'The Writing''s on the Wall', '{"Darkchild"}');
+('Hanson');
+
+INSERT INTO songs
+(title, duration_in_seconds, release_date, album)
+VALUES
+('MMMBop', 238, '04-15-1997', 'Middle of Nowhere');
+
+INSERT INTO producers
+(name)
+VALUES
+('Dust Brothers'),
+('Stephen Lironi');
+
+INSERT INTO songs_artists_producers
+(songid, artistid, producerid)
+VALUES
+(1,1,1),
+(1,1,2);
+
