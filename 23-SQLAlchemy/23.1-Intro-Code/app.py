62b4aed199d94abdbbc74533aaaa539515d1ecba
James Hudnall <jimmyhudnall@gmail.com>
2022-01-26 07:19:38 -0500

    Add lesson code

--- 23-SQLAlchemy/23.1-Intro-Code/app.py
+++ 23-SQLAlchemy/23.1-Intro-Code/app.py
@@ -1,14 +1,46 @@
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, Pet
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///pet_shop_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = 'ajklhsdkljaskldalskdjalksjd123jlk12j3'
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
+connect_db(app)
+
 
 @app.route('/')
-def home_page():
-    '''Show home page'''
-    return render_template('home.html')
+def list_pets():
+    '''Show list of all pets in db'''
+    pets = Pet.query.all()
+    return render_template('list.html', pets=pets)
+
+
+@app.route('/', methods=['POST'])
+def create_pet():
+    name = request.form.get('name')
+    species = request.form.get('species')
+    hunger = request.form.get('hunger')
+    hunger = int(hunger) if hunger else None
+
+    pet = Pet(name=name, species=species, hunger=hunger)
+    db.session.add(pet)
+    db.session.commit()
+    return redirect(f'/{pet.id}')
+
+
+@app.route('/<int:pet_id>')
+def show_pet(pet_id):
+    '''Show details about a single pet'''
+    pet = Pet.query.get_or_404(pet_id)
+    return render_template('details.html', pet=pet)
+
+
+@app.route('/species/<species_id>')
+def show_pets_by_species(species_id):
+    pets = Pet.get_by_species(species_id)
+    return render_template('species.html', pets=pets, species=species_id)
