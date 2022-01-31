dcac62f804623b216d37dc942d75f98bef71fab5
James Hudnall <jimmyhudnall@gmail.com>
2022-01-31 09:04:27 -0500

    Implement Part One

--- 24-Flask-Intermediate/24.3-Exercise-Flask-Cupcakes/app.py
+++ 24-Flask-Intermediate/24.3-Exercise-Flask-Cupcakes/app.py
@@ -1,1 +1,12 @@
 """Flask app for Cupcakes"""
+from flask import Flask, request, redirect, jsonify, render_template
+from models import Cupcake, db, connect_db
+from configuration import FLASK_SECRET
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = FLASK_SECRET
+
+connect_db(app)
--- 24-Flask-Intermediate/24.3-Exercise-Flask-Cupcakes/models.py
+++ 24-Flask-Intermediate/24.3-Exercise-Flask-Cupcakes/models.py
@@ -1,1 +1,21 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+
+class Cupcake(db.Model):
+    ''''Cupcake Model'''
+
+    __tablename__ = 'cupcakes'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.Text, nullable=False,
+                      default='https://tinyurl.com/demo-cupcake')
