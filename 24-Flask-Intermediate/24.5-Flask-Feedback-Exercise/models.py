b32dfb8490311488c2e2c3b81edce4b12f96d696
James Hudnall <jimmyhudnall@gmail.com>
2022-02-02 07:28:02 -0500

    Implement part 7

--- 24-Flask-Intermediate/24.5-Flask-Feedback-Exercise/models.py
+++ 24-Flask-Intermediate/24.5-Flask-Feedback-Exercise/models.py
@@ -37,4 +37,12 @@ class User(db.Model):
             return False
 
 
-bcrypt.check_password_hash
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    username = db.Column(db.String(20), db.ForeignKey('users.username'))
+
+    user = db.relationship('User', backref='Feedback')
--- 24-Flask-Intermediate/24.5-Flask-Feedback-Exercise/seed.sql
+++ 24-Flask-Intermediate/24.5-Flask-Feedback-Exercise/seed.sql
@@ -1,10 +1,26 @@
 from app import app
-from models import db, User
+from models import db, User, Feedback
 
-DROP DATABASE IF EXISTS flask_feedback;
-CREATE DATABASE flask_feedback;
 
 db.drop_all()
 db.create_all()
 
--- u1 = User(username='monkeyguy1', password)
+user_1 = User.register(
+  username='Ralph57',
+  password='password123',
+  email='Ralph@gmail.com',
+  first_name='Ralph',
+  last_name='Johnson'
+)
+
+db.session.add(user_1)
+db.session.commit()
+
+feedback_1 = Feedback(
+  title='My Feedback',
+  content='Regarding my feedback, these are my thoughts...',
+  username='Ralph57'
+)
+
+db.session.add(feedback_1)
+db.session.commit()
