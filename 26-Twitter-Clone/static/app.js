410ce179e4cadc7b6732ca0c919e7cf5f395e754
James Hudnall <jimmyhudnall@gmail.com>
2022-02-07 10:18:20 -0500

    Implement AJAX handling of message likes

--- 26-Twitter-Clone/static/app.js
+++ 26-Twitter-Clone/static/app.js
@@ -7,6 +7,8 @@ $(document).ready(function() {
 	$('#newMessage').on('hidden.bs.modal', function() {
 		$('#modal-body').empty();
 	});
+
+	$('#messages').on('click', '#likeBtn', likeMessage);
 });
 
 async function getMsgFormHTML() {
@@ -15,4 +17,19 @@ async function getMsgFormHTML() {
 		method: 'GET'
 	});
 	return res.data;
+}
+
+async function likeMessage(e) {
+	e.preventDefault();
+	const $tgt = $(e.target);
+	const msgid = $tgt.closest('li').data().msgid;
+	try {
+		const res = await axios({
+			url: `/users/add_like/${msgid}`,
+			method: 'POST'
+		});
+	} catch (err) {
+		alert(`Something went wrong with the API. Info: ${err}`);
+	}
+	$tgt.closest('i').toggleClass('fas far');
 }
