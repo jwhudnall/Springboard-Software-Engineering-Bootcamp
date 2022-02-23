71a0b5d172a34ae10200d9f6c341fa8feaec7226
James Hudnall <jimmyhudnall@gmail.com>
2022-02-23 14:11:23 -0500

    Add additional tests

--- 31-Node-Fundamentals/31.5-Jest-Testing/31.5-Markov-Exercise/markov.test.js
+++ 31-Node-Fundamentals/31.5-Jest-Testing/31.5-Markov-Exercise/markov.test.js
@@ -12,12 +12,24 @@ describe("Test MarkovMachine", () => {
     expect(Array.isArray(mm.words)).toBeTruthy();
   });
 
+  test("choice should select valid option from array", () => {
+    const arr = ["a", "b", "c"];
+    const choice = MarkovMachine.choice(arr);
+    expect(arr).toContain(choice);
+  });
+
   test("Should contain chains map with appropriate keys", () => {
     expect(mm.chains.get("the")).toBeTruthy();
     expect(mm.chains.get("cat")).toBeTruthy();
     expect(mm.chains.get("in")).toBeTruthy();
     expect(mm.chains.get("hat")).toBeTruthy();
     expect(mm.chains.get("is")).toBeTruthy();
+  });
+
+  test("Should generate semi-predictable text", () => {
+    const text = mm.makeText();
+    console.log(`Text: ${text}`);
+    expect(text.endsWith("hat.")).toBe(true);
   });
 
   test("Chains keys should contain correct array values", () => {
