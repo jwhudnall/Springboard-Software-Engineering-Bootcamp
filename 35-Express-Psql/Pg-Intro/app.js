cf4f25151df80be1384b3ace5225e2662c6ac697
James Hudnall <jimmyhudnall@gmail.com>
2022-03-02 19:33:26 -0500

    Create starter files

---
+++ 35-Express-Psql/Pg-Intro/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const ExpressError = require("./expressError");
+
+app.use(express.json());
+
+const uRoutes = require("./routes/users");
+app.use("/users", uRoutes);
+
+// 404 Handler
+app.use((req, res, next) => {
+  const err = new ExpressError("Not Found", 404);
+  return next(err);
+});
+
+// Generic error handler
+app.use((err, req, res, next) => {
+  let status = err.status || 500;
+  return res.status(status).json({
+    error: {
+      message: err.message,
+      status: status,
+    },
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000...");
+});
