a7ca6ebd96fa19dc1d486e56a9e1b669c1dbcf26
James Hudnall <jimmyhudnall@gmail.com>
2022-03-02 20:52:42 -0500

    Add db and initial routes

--- 35-Express-Psql/Pg-Intro/routes/users.js
+++ 35-Express-Psql/Pg-Intro/routes/users.js
@@ -1,4 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../db");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const results = await db.query(`SELECT * FROM users`);
+    return res.json(results.rows);
+  } catch (e) {
+    return next(e);
+  }
+});
+
+router.get("/search", async (req, res, next) => {
+  try {
+    const { type } = req.query;
+    const results = await db.query(`SELECT * FROM users WHERE type=$1`, [type]);
+    return res.json(results.rows);
+  } catch (e) {
+    return next(e);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const { name, type } = req.body;
+    const results = await db.query(
+      "INSERT INTO users (name, type) VALUES ($1, $2) RETURNING *",
+      [name, type]
+    );
+    return res.status(201).json(results.rows);
+  } catch (e) {
+    return next(e);
+  }
+});
 
 module.exports = router;
