2713801592b1b5c20c3873b023430f310a389b91
James Hudnall <jimmyhudnall@gmail.com>
2022-03-08 09:39:01 -0500

    Add save() method to Reservation class

--- 35-Express-Psql/35.3-Reservations-App/models/reservation.js
+++ 35-Express-Psql/35.3-Reservations-App/models/reservation.js
@@ -4,11 +4,10 @@ const moment = require("moment");
 
 const db = require("../db");
 
-
 /** A reservation for a party */
 
 class Reservation {
-  constructor({id, customerId, numGuests, startAt, notes}) {
+  constructor({ id, customerId, numGuests, startAt, notes }) {
     this.id = id;
     this.customerId = customerId;
     this.numGuests = numGuests;
@@ -19,26 +18,43 @@ class Reservation {
   /** formatter for startAt */
 
   getformattedStartAt() {
-    return moment(this.startAt).format('MMMM Do YYYY, h:mm a');
+    return moment(this.startAt).format("MMMM Do YYYY, h:mm a");
   }
 
   /** given a customer id, find their reservations. */
 
   static async getReservationsForCustomer(customerId) {
     const results = await db.query(
-          `SELECT id, 
-           customer_id AS "customerId", 
-           num_guests AS "numGuests", 
-           start_at AS "startAt", 
+      `SELECT id,
+           customer_id AS "customerId",
+           num_guests AS "numGuests",
+           start_at AS "startAt",
            notes AS "notes"
-         FROM reservations 
+         FROM reservations
          WHERE customer_id = $1`,
-        [customerId]
+      [customerId]
     );
 
-    return results.rows.map(row => new Reservation(row));
+    return results.rows.map((row) => new Reservation(row));
+  }
+  /** Save this registration */
+  async save() {
+    if (this.id === undefined) {
+      const result = await db.query(
+        `INSERT INTO reservations (customer_id, start_at, num_guests, notes)
+             VALUES ($1, $2, $3, $4)
+             RETURNING id`,
+        [this.customerId, this.startAt, this.numGuests, this.notes]
+      );
+      this.id = result.rows[0].id;
+    } else {
+      await db.query(
+        `UPDATE reservations SET start_at=$1, num_guests=$2, notes=$3
+             WHERE id=$4`,
+        [this.startAt, this.numGuests, this.notes, this.id]
+      );
+    }
   }
 }
-
 
 module.exports = Reservation;
