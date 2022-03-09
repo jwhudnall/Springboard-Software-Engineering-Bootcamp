b700a771f86fb32a9e950ff57a2809c9dd4841a6
James Hudnall <jimmyhudnall@gmail.com>
2022-03-09 07:20:44 -0500

    Add comment

--- 36-Intermediate-Express/36.1-Codealong/VideoCode/routes/auth.js
+++ 36-Intermediate-Express/36.1-Codealong/VideoCode/routes/auth.js
@@ -9,11 +9,11 @@ const jwt = require("jsonwebtoken");
 const { BCRYPT_WORK_FACTOR, SECRET_KEY } = require("../config");
 const { ensureLoggedIn, ensureAdmin } = require("../middleware/auth");
 
-router.get('/', (req, res, next) => {
-  res.send("APP IS WORKING!!!")
-})
+router.get("/", (req, res, next) => {
+  res.send("APP IS WORKING!!!");
+});
 
-router.post('/register', async (req, res, next) => {
+router.post("/register", async (req, res, next) => {
   try {
     const { username, password } = req.body;
     if (!username || !password) {
@@ -22,63 +22,62 @@ router.post('/register', async (req, res, next) => {
     // hash password
     const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
     // save to db
-    const results = await db.query(`
+    const results = await db.query(
+      `
       INSERT INTO users (username, password)
       VALUES ($1, $2)
       RETURNING username`,
-      [username, hashedPassword]);
+      [username, hashedPassword]
+    );
     return res.json(results.rows[0]);
   } catch (e) {
-    if (e.code === '23505') {
+    if (e.code === "23505") {
+      // postgres code for unique_violation
       return next(new ExpressError("Username taken. Please pick another!", 400));
     }
-    return next(e)
+    return next(e);
   }
 });
 
-router.post('/login', async (req, res, next) => {
+router.post("/login", async (req, res, next) => {
   try {
     const { username, password } = req.body;
     if (!username || !password) {
       throw new ExpressError("Username and password required", 400);
     }
     const results = await db.query(
-      `SELECT username, password 
+      `SELECT username, password
        FROM users
        WHERE username = $1`,
-      [username]);
+      [username]
+    );
     const user = results.rows[0];
     if (user) {
       if (await bcrypt.compare(password, user.password)) {
         const token = jwt.sign({ username }, SECRET_KEY);
-        return res.json({ message: `Logged in!`, token })
+        return res.json({ message: `Logged in!`, token });
       }
     }
     throw new ExpressError("Invalid username/password", 400);
   } catch (e) {
     return next(e);
   }
-})
+});
 
-router.get('/topsecret',
-  ensureLoggedIn,
-  (req, res, next) => {
-    try {
-      return res.json({ msg: "SIGNED IN! THIS IS TOP SECRET.  I LIKE PURPLE." })
+router.get("/topsecret", ensureLoggedIn, (req, res, next) => {
+  try {
+    return res.json({ msg: "SIGNED IN! THIS IS TOP SECRET.  I LIKE PURPLE." });
+  } catch (e) {
+    return next(new ExpressError("Please login first!", 401));
+  }
+});
 
-    } catch (e) {
-      return next(new ExpressError("Please login first!", 401))
-    }
-  })
+router.get("/private", ensureLoggedIn, (req, res, next) => {
+  return res.json({ msg: `Welcome to my VIP section, ${req.user.username}` });
+});
 
-router.get('/private', ensureLoggedIn, (req, res, next) => {
-  return res.json({ msg: `Welcome to my VIP section, ${req.user.username}` })
-})
-
-router.get('/adminhome', ensureAdmin, (req, res, next) => {
-  return res.json({ msg: `ADMIN DASHBOARD! WELCOME ${req.user.username}` })
-})
-
+router.get("/adminhome", ensureAdmin, (req, res, next) => {
+  return res.json({ msg: `ADMIN DASHBOARD! WELCOME ${req.user.username}` });
+});
 
 module.exports = router;
-
