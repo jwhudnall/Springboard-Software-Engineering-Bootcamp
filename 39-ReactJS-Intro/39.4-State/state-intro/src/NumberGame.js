fff500c36ac20bf72e65f66885d56f5fd2a5c651
James Hudnall <jimmyhudnall@gmail.com>
2022-03-23 13:32:39 -0400

    Add restart button to NumberGame

--- 39-ReactJS-Intro/39.4-State/state-intro/src/NumberGame.js
+++ 39-ReactJS-Intro/39.4-State/state-intro/src/NumberGame.js
@@ -3,16 +3,28 @@ import "./NumberGame.css";
 
 const NumberGame = (props) => {
   const genRandom = () => Math.floor(Math.random() * 10) + 1;
-  const [guess, setGuess] = useState(genRandom());
+  const restart = () => {
+    setTarget(genRandom());
+    setGuess(0);
+    setGuessCount(0);
+  };
+  const makeGuess = () => {
+    setGuess(genRandom());
+    setGuessCount(guessCount + 1);
+  };
+  const [guess, setGuess] = useState(0);
   const [target, setTarget] = useState(genRandom());
+  const [guessCount, setGuessCount] = useState(0);
   const isWinner = target === guess;
   return (
     <div className='NumberGame'>
       <h1>Target Num: {target}</h1>
       <h1 className={isWinner ? "winner" : "loser"}>Your Guess: {guess}</h1>
+      <h4>Guess #: {guessCount}</h4>
       {/* 2 Approaches to conditional display: */}
-      {isWinner ? null : <button onClick={() => setGuess(genRandom())}>Generate Num</button>}
-      {isWinner && <button onClick={() => setGuess(genRandom())}>Generate Num</button>}
+      {isWinner ? null : <button onClick={() => makeGuess()}>Generate Num</button>}
+      {/* {isWinner && <button onClick={() => setGuess(genRandom())}>Generate Num</button>} */}
+      <button onClick={restart}>New Game</button>
     </div>
   );
 };
