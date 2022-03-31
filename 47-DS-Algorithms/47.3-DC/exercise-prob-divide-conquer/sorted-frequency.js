6cbe88440386b95dec89e3d4334fd8622f819782
James Hudnall <jimmyhudnall@gmail.com>
2022-03-31 10:58:32 -0400

    Add solution to sorted-frequency

--- 47-DS-Algorithms/47.3-DC/exercise-prob-divide-conquer/sorted-frequency.js
+++ 47-DS-Algorithms/47.3-DC/exercise-prob-divide-conquer/sorted-frequency.js
@@ -1,5 +1,36 @@
-function sortedFrequency() {
+function sortedFrequency(arr, target) {
+  // Inputs: (array, number)
+  // Output: Number
+  // EC:
+  // Constraints: O(log n)
 
+  // split array in half until one of the target numbers is found; search left while number, search right while number.
+  let left = 0;
+  let right = arr.length - 1;
+  while (right >= left) {
+    const midIdx = Math.floor((left + right) / 2);
+    const guess = arr[midIdx];
+    if (guess < target) {
+      left = midIdx + 1;
+    } else if (guess > target) {
+      right = midIdx - 1;
+    } else {
+      // count occurrences in each direction, and return
+      let count = 0;
+      let findLeftOfIdx = midIdx;
+      let findRightOfIdx = midIdx + 1;
+      while (arr[findLeftOfIdx] === target) {
+        count++;
+        findLeftOfIdx--;
+      }
+      while (arr[findRightOfIdx] == target) {
+        count++;
+        findRightOfIdx++;
+      }
+      return count;
+    }
+  }
+  return -1;
 }
 
-module.exports = sortedFrequency
+module.exports = sortedFrequency;
