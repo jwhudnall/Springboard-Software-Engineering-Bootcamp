917854d613a76700c2550f13f3cddb3acad6343c
James Hudnall <jimmyhudnall@gmail.com>
2022-03-31 18:01:39 -0400

    Add solution for find-rotation-count

--- 47-DS-Algorithms/47.3-DC/exercise-prob-divide-conquer/find-rotation-count.js
+++ 47-DS-Algorithms/47.3-DC/exercise-prob-divide-conquer/find-rotation-count.js
@@ -1,5 +1,26 @@
-function findRotationCount() {
-  
+function findRotationCount(arr) {
+  // input: array
+  // output: number
+  // EC:
+  // C: O(log n)
+
+  // Find the pivot (where n < n -1). Return pivot index, if found. Else, return 0.
+  let left = 0;
+  let right = arr.length - 1;
+  while (left <= right) {
+    const midIdx = Math.floor((left + right) / 2);
+    const guess = arr[midIdx];
+    const previous = arr[midIdx - 1];
+    if (guess < previous) {
+      return midIdx;
+    } else {
+      left = midIdx + 1;
+    }
+  }
+  return 0;
 }
 
-module.exports = findRotationCount
+const result = findRotationCount([7, 9, 11, 12, 15]);
+console.log(result);
+
+module.exports = findRotationCount;
