bfebcb2539ebd6b9eb2ddfb3957e3c23ec5fb6a8
James Hudnall <jimmyhudnall@gmail.com>
2022-03-31 18:27:03 -0400

    Add solution to find-floor

--- 47-DS-Algorithms/47.3-DC/exercise-prob-divide-conquer/find-floor.js
+++ 47-DS-Algorithms/47.3-DC/exercise-prob-divide-conquer/find-floor.js
@@ -1,5 +1,27 @@
-function findFloor() {
-  
+function findFloor(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  if (target < arr[0]) {
+    return -1;
+  }
+  if (target > arr[right]) {
+    return arr[right];
+  }
+
+  while (left < right) {
+    const midIdx = Math.floor((left + right) / 2);
+    const guess = arr[midIdx];
+    if (guess > target) {
+      right = midIdx - 1;
+    } else if (guess <= target && arr[midIdx + 1] > target) {
+      return guess;
+    } else {
+      left = midIdx;
+    }
+  }
 }
 
-module.exports = findFloor
+const result = findFloor([1, 2, 8, 10, 10, 12, 19], 0);
+console.log(result);
+module.exports = findFloor;
