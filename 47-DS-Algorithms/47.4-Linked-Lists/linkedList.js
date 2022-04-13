0b2a601054c4b6f6c2cae91ccdd8b38d2fdaa6ba
James Hudnall <jimmyhudnall@gmail.com>
2022-04-13 11:31:55 -0400

    Add LinkedList Class with traverse(), find() methods

--- 47-DS-Algorithms/47.4-Linked-Lists/linkedList.js
+++ 47-DS-Algorithms/47.4-Linked-Lists/linkedList.js
@@ -5,6 +5,29 @@ class Node {
   }
 }
 
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  traverse() {
+    let currentNode = this.head;
+    while (currentNode) {
+      console.log(currentNode.val);
+      currentNode = currentNode.next;
+    }
+  }
+  find(val) {
+    let currentNode = this.head;
+    while (currentNode) {
+      if (currentNode.val === val) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
+  }
+}
+
 let antNode = new Node("ant");
 let beeNode = new Node("bee");
 let caterpillarNode = new Node("caterpillar");
@@ -13,3 +36,9 @@ antNode.next = beeNode;
 beeNode.next = caterpillarNode;
 
 console.log(antNode.next.next); // {'val': 'caterpillar', 'next': null}
+
+// Page Example
+const firstPage = new Node("google.com", new Node("reddit.com", new Node("amazon.com")));
+
+const history = new LinkedList();
+history.head = firstPage;
