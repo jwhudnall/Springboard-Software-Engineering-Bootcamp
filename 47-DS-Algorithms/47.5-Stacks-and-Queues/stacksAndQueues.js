3ca1e578870985bd0aa41448fe2732232181f58c
James Hudnall <jimmyhudnall@gmail.com>
2022-04-14 21:35:15 -0400

    Add (sub-optimal) PriorityQueue Class implementation

--- 47-DS-Algorithms/47.5-Stacks-and-Queues/stacksAndQueues.js
+++ 47-DS-Algorithms/47.5-Stacks-and-Queues/stacksAndQueues.js
@@ -1,3 +1,4 @@
+// Queues
 class Queue {
   constructor() {
     this.data = {};
@@ -9,3 +10,31 @@ class Queue {
     return this.data.shift();
   }
 }
+
+// Priority Queue
+class PriorityQueue {
+  constructor() {
+    this.data = [];
+  }
+  add(priority, value) {
+    this.data.push({ priority, value });
+  }
+  poll() {
+    let maxIdx = 0;
+    let maxPriority = this.data[0].priority;
+    for (let i = 1; i < this.data.length; i++) {
+      const curPriority = this.data[i].priority;
+      if (curPriority > maxPriority) {
+        maxPriority = curPriority;
+        maxIdx = i;
+      }
+    }
+    return this.data.splice(maxIdx, 1)[0].value;
+  }
+}
+
+const pq = new PriorityQueue();
+pq.add(8, "broken leg");
+pq.add(6, "dizziness");
+pq.add(10, "heart attack");
+pq.add(2, "tooth ache");
