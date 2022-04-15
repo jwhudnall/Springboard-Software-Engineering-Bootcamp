4904b89f6d85c1c04b3fcbbe3de43bb49a93c2b8
James Hudnall <jimmyhudnall@gmail.com>
2022-04-15 08:03:59 -0400

    Implement Queue Class

--- 47-DS-Algorithms/47.5-Stacks-and-Queues/dsa-stacks-queues/queue.js
+++ 47-DS-Algorithms/47.5-Stacks-and-Queues/dsa-stacks-queues/queue.js
@@ -20,26 +20,42 @@ class Queue {
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
   enqueue(val) {
-
+    const newNode = new Node(val);
+    if (!this.last) {
+      this.first = newNode;
+    } else {
+      this.last.next = newNode;
+    }
+    this.last = newNode;
+    this.size++;
   }
 
   /** dequeue(): remove the node from the start of the queue
    * and return its value. Should throw an error if the queue is empty. */
 
   dequeue() {
-
+    if (!this.last) {
+      throw new Error("Queue is empty.");
+    }
+    const target = this.first.val;
+    this.first = this.first.next;
+    this.size--;
+    return target;
   }
 
   /** peek(): return the value of the first node in the queue. */
 
   peek() {
-
+    if (!this.first) {
+      return null;
+    }
+    return this.first.val;
   }
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
   isEmpty() {
-
+    return this.size === 0;
   }
 }
 
