5c01ede0777e5a74e6df3706a3aa4f056d1fb5b4
James Hudnall <jimmyhudnall@gmail.com>
2022-04-15 14:02:51 -0400

    Implement Stack Class

--- 47-DS-Algorithms/47.5-Stacks-and-Queues/dsa-stacks-queues/stack.js
+++ 47-DS-Algorithms/47.5-Stacks-and-Queues/dsa-stacks-queues/stack.js
@@ -20,26 +20,50 @@ class Stack {
   /** push(val): add new value to end of the stack. Returns undefined. */
 
   push(val) {
-
+    const newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+      this.size++;
+    } else {
+      newNode.next = this.first;
+      this.first = newNode;
+      this.size++;
+    }
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
-
+    if (!this.first) {
+      throw new Error("Stack is empty.");
+    }
+    const val = this.first.val;
+    if (this.first === 1) {
+      this.first = null;
+      this.last = null;
+      this.size--;
+    } else {
+      this.first = this.first.next;
+      this.size--;
+    }
+    return val;
   }
 
   /** peek(): return the value of the first node in the stack. */
 
   peek() {
-
+    if (!this.first) {
+      throw new Error("Stack is empty.");
+    }
+    return this.first.val;
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    return this.size === 0;
   }
 }
 
