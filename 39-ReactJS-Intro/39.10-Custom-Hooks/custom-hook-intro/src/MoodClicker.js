ad806f90376cbcf1e65a62b9a992c8e48a311773
James Hudnall <jimmyhudnall@gmail.com>
2022-04-16 20:36:22 -0400

    Add mood/dark-light mode toggle features

---
+++ 39-ReactJS-Intro/39.10-Custom-Hooks/custom-hook-intro/src/MoodClicker.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import "./MoodClicker.css";
+
+const MoodClicker = () => {
+  const [isHappy, setIsHappy] = useState(true);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const toggleMood = () => {
+    setIsHappy((mood) => !mood);
+  };
+  const toggleIsDarkMode = () => {
+    setIsDarkMode((mode) => !mode);
+  };
+  return (
+    <div className={isDarkMode ? "Clicker-dark" : "Clicker-light"}>
+      <h1>{isHappy ? "😀" : "😢"}</h1>
+      <button onClick={toggleMood}>Change Mood</button>
+      <button onClick={toggleIsDarkMode}>Toggle Dark/Light Mode</button>
+    </div>
+  );
+};
+
+export default MoodClicker;
