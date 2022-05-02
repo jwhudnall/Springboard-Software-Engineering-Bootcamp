5b151b5084d176bc04087e4350d44c2c58038ca5
James Hudnall <jimmyhudnall@gmail.com>
2022-05-02 19:15:43 -0400

    Add class-based counter

--- 41-React-Router-History/41.3-React-History/class-comps/src/Counter.js
+++ 41-React-Router-History/41.3-React-History/class-comps/src/Counter.js
@@ -3,12 +3,15 @@ import React from "react";
 class Counter extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { count: 0, isHiding: false };
     console.log(this.props);
   }
   render() {
     return (
       <div>
         <h1>I am a counter</h1>
+        <h3>Count is: {this.state.count}</h3>
+        <button onClick={() => this.setState(this.state.count + 1)}>Add 1</button>
       </div>
     );
   }
