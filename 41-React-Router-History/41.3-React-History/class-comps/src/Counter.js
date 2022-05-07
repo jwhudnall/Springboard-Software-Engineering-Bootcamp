04666bdaceb1a393b02814a1a582d30d3d2ac36c
James Hudnall <jimmyhudnall@gmail.com>
2022-05-07 17:55:40 -0400

    Add Class methods increment, decrement

--- 41-React-Router-History/41.3-React-History/class-comps/src/Counter.js
+++ 41-React-Router-History/41.3-React-History/class-comps/src/Counter.js
@@ -4,15 +4,23 @@ class Counter extends React.Component {
   constructor(props) {
     super(props);
     this.state = { count: 0, isHiding: false };
-    console.log(this.props);
+    // "binds" needed to use this
+    this.decrement = this.decrement.bind(this);
+    this.increment = this.increment.bind(this);
+  }
+  increment() {
+    this.setState({ count: this.state.count + 1 });
+  }
+  decrement() {
+    this.setState({ count: this.state.count - 1 });
   }
   render() {
     return (
       <div>
         <h1>I am a counter</h1>
         <h3>Count is: {this.state.count}</h3>
-        <button onClick={() => this.setState({ count: this.state.count + 1 })}>Add 1</button>
-        <button onClick={() => this.setState({ count: this.state.count - 1 })}>Subtract 1</button>
+        <button onClick={this.increment}>Add 1</button>
+        <button onClick={this.decrement}>Subtract 1</button>
       </div>
     );
   }
